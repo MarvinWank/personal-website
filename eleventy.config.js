b0a6ec929cb55f552c8f24f4f688c786549200e4
m.wank <m.wank@cis-solutions.eu>
2025-01-07 12:52:23 +0100

    perfects layout

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,20 @@
+const fs = require("fs");
+const path = require("path");
+const nunjucks = require("nunjucks");
+
 module.exports = function (eleventyConfig) {
     // Output directory: _site
 
     // Copy `img/` to `_site/img`
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("img");
+
+    eleventyConfig.addShortcode("link", (href, text) => {
+        const templatePath = path.join(__dirname, "_includes", "components", "link.njk");
+        const template = fs.readFileSync(templatePath, "utf-8");
+
+        return nunjucks.renderString(template, { href, text });
+    });
 
     // // Copy `css/fonts/` to `_site/css/fonts`
     // // Keeps the same directory structure.
